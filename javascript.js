26a33d6d9bd58994915fd237355433979e06ce91
jerome310 <jeromeyoung333@gmail.com>
2023-06-30 14:52:29 -0400

    added the functionality of adding the data to the application

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let adviceCounter = document.querySelector(".advice-counter");
+const text = document.querySelector(".text");
+const btn = document.querySelector(".dice-container");
+const url = "https://api.adviceslip.com/advice";
+let placeHolder = '1';
+
+let advice = () => {
+    text.classList.remove("fade");
+    fetch(url)
+      .then((data) => data.json())
+      .then((item) => {
+        text.textContent = `${item.slip.advice}`;
+        adviceCounter.textContent = placeHolder++;
+        text.classList.add("fade");
+    });
+};
+
+btn.addEventListener('click', advice)
+
+advice();
